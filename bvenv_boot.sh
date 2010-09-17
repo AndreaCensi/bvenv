0e3f02564527f5124b09e559c8424157ebd6b973
Andrea Censi <andrea@cds.caltech.edu>
2010-09-16 19:35:50 -0700

    docs

--- bvenv_boot.sh
+++ bvenv_boot.sh
@@ -34,60 +34,72 @@ ${BVENV_PREFIX}/bin/easy_install nose
 echo "Installing a yaml library (for patience)"
 ${BVENV_PREFIX}/bin/easy_install pyyaml
 
-
-echo "The next steps can be done with patience. (./patience resources.yaml checkout)"
-read
-
-
-echo Checkout the sources
-read
-cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/camera-bootstrap
-cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/snp-log
-cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/snp_geometry
-cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/albert-sp10
-cd ${BVENV_SRC} && git clone git@github.com:AndreaCensi/compmake.git
-cd ${BVENV_SRC} && git clone git@github.com:AndreaCensi/json-c.git
-cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/pri/bv/src bv
-cd ${BVENV_SRC} && git clone git@github.com:AndreaCensi/reprep.git
+echo "Install patience"
+pip install -e "git+git@github.com:AndreaCensi/patience.git#egg=Patience"
 
 
-cd ${BVENV_SRC}          && svn up snp-log
-cd ${BVENV_SRC}          && svn up snp_geometry
-cd ${BVENV_SRC}          && svn up camera-bootstrap
-cd ${BVENV_SRC}          && svn up albert-sp10
-cd ${BVENV_SRC}/compmake && git pull
-cd ${BVENV_SRC}/json-c   && git pull
-cd ${BVENV_SRC}/reprep   && git pull
-cd ${BVENV_SRC}          && svn up bv
+patience checkout
+
+patience install
 
 
-cd ${BVENV_SRC}          && svn commit snp-log
-cd ${BVENV_SRC}          && svn commit snp_geometry
-cd ${BVENV_SRC}          && svn commit camera-bootstrap
-cd ${BVENV_SRC}          && svn commit albert-sp10
-cd ${BVENV_SRC}/compmake && git commit -a
-cd ${BVENV_SRC}/json-c   && git commit -a
-cd ${BVENV_SRC}          && svn commit bv
-
-cd ${BVENV_SRC}/snp_geometry             && python setup.py develop
-cd ${BVENV_SRC}/snp-log                  && python setup.py develop
-cd ${BVENV_SRC}/albert-sp10/camera_utils && python setup.py develop
-cd ${BVENV_SRC}/compmake                 && python setup.py develop
-cd ${BVENV_SRC}/camera-bootstrap         && python setup.py develop
-cd ${BVENV_SRC}/reprep                   && python setup.py develop
-
-cd ${BVENV_SRC}/json-c        && cmake -DCMAKE_INSTALL_PREFIX=${BVENV_PREFIX} . && make install
-cd ${BVENV_SRC}/bv/raytracer  && cmake -DCMAKE_INSTALL_PREFIX=${BVENV_PREFIX} . && make install
-cd ${BVENV_SRC}/bv/jsonstream && python setup.py develop
-cd ${BVENV_SRC}/bv            && python setup.py develop
-
-cd ${BVENV_SRC}/bv            && nosetests
-cd ${BVENV_SRC}/compmake            && nosetests
+# mkdir -p ${BVENV_DATA}
+# tokyo=131.215.42.218
+# #cd ${BVENV_DATA} && rsync --progress -a -v andrea@tokyo:BIGDATA/er1-logs .
+# cd ${BVENV_DATA} && rsync --progress -a -v andrea@131.215.42.218:BIGDATA/er1-logs .
 
 
-mkdir -p ${BVENV_DATA}
 
-tokyo=131.215.42.218
-#cd ${BVENV_DATA} && rsync --progress -a -v andrea@tokyo:BIGDATA/er1-logs .
-cd ${BVENV_DATA} && rsync --progress -a -v andrea@131.215.42.218:BIGDATA/er1-logs .
+
+# echo "The next steps can be done with patience. (./patience resources.yaml checkout)"
+# read
+# 
+# 
+# echo Checkout the sources
+# read
+# cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/camera-bootstrap
+# cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/snp-log
+# cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/snp_geometry
+# cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/snp/albert-sp10
+# cd ${BVENV_SRC} && git clone git@github.com:AndreaCensi/compmake.git
+# cd ${BVENV_SRC} && git clone git@github.com:AndreaCensi/json-c.git
+# cd ${BVENV_SRC} && svn co https://www.cds.caltech.edu/subversion/andrea/pri/bv/src bv
+# cd ${BVENV_SRC} && git clone git@github.com:AndreaCensi/reprep.git
+# 
+# 
+# cd ${BVENV_SRC}          && svn up snp-log
+# cd ${BVENV_SRC}          && svn up snp_geometry
+# cd ${BVENV_SRC}          && svn up camera-bootstrap
+# cd ${BVENV_SRC}          && svn up albert-sp10
+# cd ${BVENV_SRC}/compmake && git pull
+# cd ${BVENV_SRC}/json-c   && git pull
+# cd ${BVENV_SRC}/reprep   && git pull
+# cd ${BVENV_SRC}          && svn up bv
+# 
+# 
+# cd ${BVENV_SRC}          && svn commit snp-log
+# cd ${BVENV_SRC}          && svn commit snp_geometry
+# cd ${BVENV_SRC}          && svn commit camera-bootstrap
+# cd ${BVENV_SRC}          && svn commit albert-sp10
+# cd ${BVENV_SRC}/compmake && git commit -a
+# cd ${BVENV_SRC}/json-c   && git commit -a
+# cd ${BVENV_SRC}          && svn commit bv
+# 
+# cd ${BVENV_SRC}/snp_geometry             && python setup.py develop
+# cd ${BVENV_SRC}/snp-log                  && python setup.py develop
+# cd ${BVENV_SRC}/albert-sp10/camera_utils && python setup.py develop
+# cd ${BVENV_SRC}/compmake                 && python setup.py develop
+# cd ${BVENV_SRC}/camera-bootstrap         && python setup.py develop
+# cd ${BVENV_SRC}/reprep                   && python setup.py develop
+# 
+# cd ${BVENV_SRC}/json-c        && cmake -DCMAKE_INSTALL_PREFIX=${BVENV_PREFIX} . && make install
+# cd ${BVENV_SRC}/bv/raytracer  && cmake -DCMAKE_INSTALL_PREFIX=${BVENV_PREFIX} . && make install
+# cd ${BVENV_SRC}/bv/jsonstream && python setup.py develop
+# cd ${BVENV_SRC}/bv            && python setup.py develop
+# 
+# cd ${BVENV_SRC}/bv            && nosetests
+# cd ${BVENV_SRC}/compmake            && nosetests
+# 
+# 
+
 
--- docs/platform.rst
+++
@@ -1,7 +0,0 @@
-Builind pymedia from source
----------------------------
-
-http://sourceforge.net/projects/pymedia/files/pymedia/pymedia-1.3.7.3/pymedia-1.3.7.3.tar.gz/download
-
-export CFLAGS='-fPIC'
-
